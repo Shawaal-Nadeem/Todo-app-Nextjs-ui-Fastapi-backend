d16fe17297a7eb01b00a07de988b67d1f74a49d7
Shawaal Nadeem <110089331+Shawaal-Nadeem@users.noreply.github.com>
2024-04-19 00:30:22 +0500

    url update

--- src/components/getTodos.tsx
+++ src/components/getTodos.tsx
@@ -3,8 +3,8 @@
 //   ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
 //   : "http://localhost:3000/api";
 
-const BaseUrl = "http://127.0.0.1:8000";
-
+// const BaseUrl = "http://127.0.0.1:8000";
+const BaseUrl = "https://sweeping-fawn-enormous.ngrok-free.app/"
 export const GetTodos=async()=>{
     
     const res = await fetch(`${BaseUrl}/todos`)
